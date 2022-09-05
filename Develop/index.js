b2ce28ec7d15f134848ddeabf6ff4e5d0905fae8
Jayita Chowdhury <jayitachowdhury@Amits-MacBook-Pro.local>
2022-09-05 12:34:52 -0700

    Added readme and modifications

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,3 @@
-
 const inquirer = require('inquirer');
 const fs = require('fs');
 const Engineer = require("./lib/Engineer");
@@ -37,29 +36,30 @@ function addMember() {
                 message: 'Enter email address:',
             }])
 
-            .then(function({name, role, id, email}) {
-                let roleInfo = '';
-                if (role === 'Engineer') {
-                    roleInfo = 'GitHub username';
-                } else if (role === 'Intern') {
-                    roleInfo = 'school name';
-                } else {
-                    roleInfo = 'office phone number';
-                };
+        .then(function ({ name, role, id, email }) {
+            let roleInfo = '';
+            if (role === 'Engineer') {
+                roleInfo = 'GitHub username';
+            } else if (role === 'Intern') {
+                roleInfo = 'school name';
+            } else {
+                roleInfo = 'office phone number';
+            };
 
-                inquirer.prompt([{
-                    name: 'role',
-                    message: `Enter team member's ${roleInfo}`
-                },
-                {
-                    type: 'list',
-                    name: 'moreMembers',
-                    message: 'Would you like to add more team members?',
-                    choices: ['yes', 'no'],
-                }])
+            inquirer.prompt([{
+                type: 'input',
+                name: 'roleInfo',
+                message: `Enter team member's ${roleInfo}`
+            },
+            {
+                type: 'list',
+                name: 'moreMembers',
+                message: 'Would you like to add more team members?',
+                choices: ['yes', 'no'],
+            }])
 
-                .then(function({roleInfo, moreMembers}) {
-                    let newMember;
+                .then(function ({ roleInfo, moreMembers }) {
+                    let newMember;                    
                     if (role === 'Engineer') {
                         newMember = new Engineer(name, id, email, roleInfo);
                     } else if (role === 'Intern') {
@@ -69,19 +69,19 @@ function addMember() {
                     }
                     employees.push(newMember);
                     addHtml(newMember)
-                    .then(function() {
-                        if (moreMembers === 'yes') {
-                            addMember();
-                        } else {
-                            endHTML();
-                        }
-                    });
+                        .then(function () {
+                            if (moreMembers === 'yes') {
+                                addMember();
+                            } else {
+                                endHTML();
+                            }
+                        });
                 });
-            });
-        }
+        });
+}
 
-        function generateHTML(){
-            const html =  `<!DOCTYPE html>
+function generateHTML() {
+    const html = `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -90,13 +90,14 @@ function addMember() {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
         crossorigin="anonymous">
     <title>Team-Profile-Generator</title>
-    <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    <link rel="stylesheet" href="../style.css">
+    
 </head>
 <body>
 <div class="container-fluid">
 <div class="row">
-    <div class="col-12 jumbotron mb-3 team-heading bg-danger">
+    <div class="col-12 jumbotron mb-3 team-heading">
             <h1 class="text-center text-white">MY TEAM</h1>
         </div>
     </div>
@@ -104,16 +105,16 @@ function addMember() {
 <div class="container">
 <div class="row">`
 
-fs.writeFile("./index.html", html, function(err) {
-    if (err) {
-        console.log(err);
-    }
-});
-console.log("start");
+    fs.writeFile("./index.html", html, function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    console.log("start");
 }
 
 function addHtml(member) {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
         const name = member.getName();
         const role = member.getRole();
         const id = member.getId();
@@ -123,79 +124,79 @@ function addHtml(member) {
             const gitHub = member.getGithub();
             data = `<div 
             class="card employee-card">
-            <div class="card-header bg-primary text-white">
+            <div class="card-header text-white">
                 <h2 class="card-title">${name}</h2>
                 <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${role}</h3>
             </div>
             <div class="card-body">
                 <ul class="list-group">
                     <li class="list-group-item">ID:${id}</li>
-                    <li class="list-group-item">Email:<a href="#" class="card-link">${email}</a></li>
-                    <li class="list-group-item">GitHub: <a href="#" class="card-link">${gitHub}</a>
+                    <li class="list-group-item">Email:<a href="mailto:name@rapidtables.com" class="card-link">${email}</a></li>
+                    <li class="list-group-item">GitHub: <a href="https://github.com/${gitHub}" class="card-link">${gitHub}</a>
                     </li>
                 </ul>
             </div>
         </div>`;
-    }    
-    else if (role === 'Intern') {
-        const school = member.getSchool();  
-          data =   ` <div class="card employee-card">
-          <div class="card-header bg-primary text-white">
+        }
+        else if (role === 'Intern') {
+            const school = member.getSchool();
+            data = ` <div class="card employee-card">
+          <div class="card-header text-white">
               <h2 class="card-title">${name}</h2>
               <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${role}</h3>
           </div>
           <div class="card-body">
               <ul class="list-group">
                   <li class="list-group-item">ID:${id}</li>
-                  <li class="list-group-item">Email: <a href="#" class="card-link">${email}</a></li>
+                  <li class="list-group-item">Email: <a href="mailto:name@rapidtables.com" class="card-link">${email}</a></li>
                   <li class="list-group-item">School: ${school}</li>
               </ul>
           </div>
       </div>`;
-    }
+        }
         else if (role === 'Manager') {
             const officeNumber = member.getOfficeNumber();
-           data = `<div
+            data = `<div
  class="row team-area col-12 d-flex justify-content-center">
             <div class="card employee-card">
-                    <div class="card-header bg-primary text-white">
+                    <div class="card-header text-white">
                         <h2 class="card-title">${name}</h2>
                         <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${role}</h3>
                     </div>
                     <div class="card-body">
                         <ul class="list-group">
                             <li class="list-group-item">ID:${id}</li>
-                            <li class="list-group-item">Email:<a href="#" class="card-link">${email}</a></li>
+                            <li class="list-group-item">Email:<a href="mailto:name@rapidtables.com" class="card-link">${email}</a></li>
                             <li class="list-group-item">Office number:${officeNumber} </li>
                         </ul>
                     </div>
                 </div> ` ;
-            }
-            console.log('Successfully adding team member');
-            fs.appendFile('index.html', data, function (err) {
-                if (err) {
-                    return reject(err);
-                };
-                return resolve();
-            });
-        });   
-        function endHTML() {
-            const endHTML = ` </div>
-            </div>
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
-
-        </body>
-        </html>`;}
-        
-            fs.appendFile('index.html', endHTML, function(err) {
-                if (err) {
-                    console.log(err);
-                };
-            });
-            console.log('end of HTML');
+        }
+        console.log('Successfully adding team member');
+        fs.appendFile('index.html', data, function (err) {
+            if (err) {
+                return reject(err);
+            };
+            return resolve();
+        });
+    });
+    fs.appendFile('index.html', endHTML, function (err) {
+        if (err) {
+            console.log(err);
         };
-        
-        init();
-        
+    });
+    console.log('end of HTML');
+};
+
+function endHTML() {
+    const endHTML = ` </div>
+        </div>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+    </body>
+    </html>`;
+}
+
+init();
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Team-Profile-Generator
+Dynamically generate a list of employees and print them to an html document.
+## Description 
+This application was created to generate a team profile based on user input using the Inquirer module from Node.js and displaying the information on a newly created html page with a style sheet. This project demonstrates use of OOP and TDD using Jest. 
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Tests](#tests)
+* [Questions](#questions)
+## Installation 
+The user should clone the repository from GitHub and download Node. This application also requires a file system and inquirer module. If testing is required, this application uses Jest. 
+## Usage 
+Use inquirer from your command line to answer questions about your project.
+View walk through video here :
+
+## License 
+This project is license under MIT
+![Github licence](http://img.shields.io/badge/license-MIT-blue.svg)
+ 
+## Tests
+Run `npm test` to run Jest for tests on constructors. 
+
+## Questions
+If you have any questions about this projects, please contact me directly at jayita.chowdhury.1987@gmail.com. You can view more of my projects at https://github.com/jayita87.
